aff81805700e7994778408e8201bc37bcaa84a76
Siddhant Gvalani <sidgvalani@vt.edu>
2025-08-15 20:49:21 -0400

    Create code.ino

---
+++ code/ControlCar/code.ino
@@ -0,0 +1,24 @@
+/*
+Sketch to control an RC Car using a web server. Sliders to control speed, and arrow buttons (up, down, left, and right) to control direction.
+*/
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+void clientRoot(){
+    Rrawliteral"(
+  
+    )rawliteral"
+  
+}
+
+void directionControl(){}
+
+void speedControl(){}
+
+void setup(){}
+
+
+void loop(){}
+
